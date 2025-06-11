fc07cfb5f4c6f9c97aa22b317779380b2c2d1752
Justin <justin@sphinxdefense.com>
2025-06-11 08:39:13 -0400

    update

--- lua/plugins/nvim-lint.lua
+++ lua/plugins/nvim-lint.lua
@@ -1,22 +1,23 @@
 return {
-	"mfussenegger/nvim-lint",
-	event = "VeryLazy",
-	keys = {
-		{
-			-- Customize or remove this keymap to your liking
-			"<leader>cL",
-			function()
-				require("lint").try_lint()
-			end,
-			mode = "n",
-			desc = "Lint buffer",
-		},
-	},
-	config = function()
-		require("lint").linters_by_ft = {
-			terraform = { "tflint" },
-			dockerfile = { "hadolint" },
-			python = { "ruff" },
-		}
-	end,
+    "mfussenegger/nvim-lint",
+    event = "VeryLazy",
+    keys = {
+        {
+            -- Customize or remove this keymap to your liking
+            "<leader>cL",
+            function()
+                require("lint").try_lint()
+            end,
+            mode = "n",
+            desc = "Lint buffer",
+        },
+    },
+    config = function()
+        require("lint").linters_by_ft = {
+            terraform = { "tflint" },
+            dockerfile = { "hadolint" },
+            python = { "ruff" },
+            dotenv = { "dotenv-linter" },
+        }
+    end,
 }
